8e3637d312ba6c422453a23938cb890e7a6ccf5f
Jeff Paulino <jp7k0d3z@gmail.com>
2021-10-22 13:09:43 -0400

    adding colorize gem for console

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,7 +1,7 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   # respond_to :json
   # before_action :configure_permitted_parameters, if: :devise_controller?
-
+  include Shared
   # before_action do
   #   I18n.locale = :en # Or whatever logic you use to choose.
   # end
@@ -15,17 +15,43 @@ class Users::RegistrationsController < Devise::RegistrationsController
     if resource.persisted?
       if resource.active_for_authentication?
         sign_up(resource_name, resource)
-        render json: { message: I18n.translate('devise.registrations.signed_up'), resource: resource }, status: 201
+        console_msg('success', 'User was successfully created')
+        render json: { message: I18n.translate('devise.registrations.signed_up'), resource: resource }, status: :created
       else
         # set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
         expire_data_after_sign_in!
         # respond_with resource, location: after_inactive_sign_up_path_for(resource)
+        console_msg('error', resource.errors)
         render json: { message: I18n.translate('devise.registrations.signed_up_but_inactive'),
                        resource: resource.errors }, status: 400
       end
     else
       clean_up_passwords resource
       set_minimum_password_length
+      render json: { message: I18n.translate('devise.failure.invalid'), resource: resource.errors }, status: 400
+      # render json: { message: I18n.translate('errors.messages.not_found'), resource: resource.errors }, status: 400
+    end
+  end
+
+  # PUT /resource
+  # We need to use a copy of the resource because we don't want to change
+  # the current user in place.
+  def update
+    self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
+    prev_unconfirmed_email = resource.unconfirmed_email if resource.respond_to?(:unconfirmed_email)
+
+    resource_updated = update_resource(resource, account_update_params)
+    yield resource if block_given?
+    if resource_updated
+      set_flash_message_for_update(resource, prev_unconfirmed_email)
+      bypass_sign_in resource, scope: resource_name if sign_in_after_change_password?
+
+      # respond_with resource, location: after_update_path_for(resource)
+      render json: { message: I18n.translate('devise.registrations.updated'), resource: resource }, status: 400
+    else
+      clean_up_passwords resource
+      set_minimum_password_length
+      # respond_with resource
       render json: { message: I18n.translate('errors.messages.not_found'), resource: resource.errors }, status: 400
     end
   end
---
+++ lib/shared_functions.rb
@@ -0,0 +1,31 @@
+require 'colorize'
+
+module Shared
+  def console_msg(level, msg)
+
+    symbols = {
+        "info" => "\u24D8",
+        "success" => "\u2713",
+        "warn" => "\u26A0",
+        "error" => "\u2622"
+    }
+
+    puts "\n "
+
+    case level
+    when "info"
+      puts "\t  #{symbols.fetch(level)} #{msg}".colorize(:light_blue).bold
+    when "success"
+      # puts "\t \u2713 #{msg}".colorize(:light_blue).bold
+      puts "\t #{symbols.fetch(level)} #{msg}".colorize(:light_blue).bold
+    when "warn"
+      puts "\t  #{symbols.fetch(level)} #{msg}".colorize(:yellow).bold
+    when "error"
+      puts "\t #{symbols.fetch(level)} #{msg}".colorize(:light_red).bold
+    else
+      puts "\t Level input does not match".colorize(:green).bold
+    end
+
+    puts "\n "
+  end
+end
